1636eae113895aac13e88f2f96c2f55f1a309e73
Amalaric <lamalaric93@gmail.com>
2022-01-09 22:51:27 +0100

    Ajout d'une fonction pour plier / déplier les grandes parties

--- Projet/scripts/main.js
+++ Projet/scripts/main.js
@@ -1,8 +1,34 @@
+//Masquer tout l'affichage si aucune recherche n'a déjà été effectuée
 function displayButtons() {
+    let main = document.getElementById("main");
     let grid = document.getElementsByClassName("ag-root-wrapper");
-    let boutons = document.getElementById("buttons");
+    let results = document.getElementById("results");
+    let stats = document.getElementById("stats");
 
     if (grid.length === 0) {
-        boutons.style.display = "none";
-    } else boutons.style.display = "flex";
+        main.style.minHeight = "80vh";
+        results.style.display = "none";
+        stats.style.display = "none";
+    } else {
+        main.style.minHeight = "auto";
+        results.style.display = "flex";
+        stats.style.display = "flex";
+    }
+}
+
+//Plier / déplier les catégories
+function toggleFold(section) {
+    let cat = document.getElementById(section);
+
+    if (section === "container-results") {
+        if (cat.style.display === "flex") {
+            cat.style.display = "none";
+        } else cat.style.display = "flex";
+    }
+    else {
+        if (cat.style.display === "block") {
+            cat.style.display = "none";
+        } else cat.style.display = "block";
+    }
+
 }
