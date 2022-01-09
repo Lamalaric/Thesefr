cb636fe220021d94784fcd80eceac63ab254417b
Amalaric <lamalaric93@gmail.com>
2022-01-09 22:51:11 +0100

    Ajout de 2 graphs + texte d'intro + corrections

--- Projet/scripts/api.php
+++ Projet/scripts/api.php
@@ -5,9 +5,9 @@ ini_set('memory_limit', '-1');
 //include('../class/Dump.php');
 //include('../connexion.php');
 //include('coord.php');
-include('load_dump.php');
+//include('load_dump.php');
 //Remove le warning du port
-error_reporting(E_ALL ^ E_WARNING);
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 $db_host = 'sqletud.u-pem.fr';
 $db_user = 'leforestier';
@@ -32,8 +32,14 @@ try {
     <link rel="stylesheet" href="../styles/style.css" type="text/css"/>
     <script type="text/javascript" src="main.js"></script>
 
+    <script src="https://kit.fontawesome.com/45e38e596f.js" crossorigin="anonymous"></script>
+
     <script src="https://unpkg.com/ag-grid-community/dist/ag-grid-community.min.js"></script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/variable-pie.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
 
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
@@ -70,10 +76,43 @@ https://www.datavis.fr/index.php?page=leaflet-firstmap
 https://opencagedata.com/tutorials/geocode-in-php
 -->
 
-<main>
+<main id="main" style="min-height: 100vh;">
 
-    <section class="results">
-        <div class="grid-wrapper">
+    <section id="welcome">
+        <h1>Thesesfr</h1>
+        <div>
+            <div class="suggest">
+                <h2>Présentation</h2>
+                <p>
+                    Theses.fr est un projet réalisé dans le cadre d'un projet PHP pour mon 3ème semestre en DUT Informatique de Marne-la-Vallée.<br><br>
+                    Commencez par rechercher une thèse selon le critère de votre choix dans la barre de recherche en haut a droite
+                </p>
+            </div>
+            <div class="mentionsL">
+                <h2>Mentions légales</h2>
+                <p>
+                    Vous trouverez les mentions légales en cliquant sur <a href="../mentions.html" target="_blank">ce lien</a>
+                </p>
+            </div>
+            <div class="me">
+                <h2>Auteur</h2>
+                <p>
+                    N'hésitez pas à visiter mon Portfolio pour voir d'autres de mes créations ou me contacter!<br>
+                    <a href="https://amalaric.dev" target="_blank">amalaric.dev</a>
+                </p>
+            </div>
+        </div>
+
+    </section>
+
+    <section id="results" class="results" style="display: flex;">
+        <div class="title" onclick="toggleFold('container-results')">
+            <h2>Résultats </h2>
+            <i class="fas fa-chevron-down"></i>
+        </div>
+        <div class="grid-wrapper" id="container-results" style="display: flex;">
+            <p id="nbResultats" style="display: block;"></p>
+            <div id="myGrid" class="ag-theme-alpine" style="display: block;"></div>
             <div id="buttons">
                 <button onclick="imprimer()">Imprimer</button>
                 <button onclick="downloadCSV()">Exporter le csv</button>
@@ -81,22 +120,32 @@ https://opencagedata.com/tutorials/geocode-in-php
                     <button>Exporter le json</button>
                 </a>
             </div>
-            <p id="nbResultats"></p>
-            <div id="myGrid" class="ag-theme-alpine"></div>
         </div>
     </section>
 
-    <section class="stats">
-        <figure class="highcharts-figure">
-            <div id="nbTheses"></div>
-            <div class="camamberts">
-                <div id="nbOnline"></div>
-                <div id="nbSoutenance"></div>
+    <section id="stats" class="stats" style="display: flex;">
+        <div class="title" onclick="toggleFold('container-stats')">
+            <h2>Statistiques</h2>
+            <i class="fas fa-chevron-down"></i>
+        </div>
+        <div id="container-stats" style="display: block;">
+            <h3>Graphiques</h3>
+            <div id="container-graphs">
+                <figure class="highcharts-figure">
+                    <div id="nbTheses"></div>
+                    <div class="camamberts">
+                        <div id="nbOnline"></div>
+                        <div id="nbSoutenance"></div>
+                        <div id="topDisciplines"></div>
+                        <div id="topLangues"></div>
+                    </div>
+                </figure>
             </div>
-        </figure>
 
-        <div class="map-container">
-            <div id="map"></div>
+            <h3>Cartographie</h3>
+            <div id="container-carto" class="map-container">
+                <div id="map"></div>
+            </div>
         </div>
 
     </section>
@@ -157,7 +206,6 @@ https://opencagedata.com/tutorials/geocode-in-php
                     //Si le site est mis en ligne, alors on met le site en lien sur le titre
                     if (params.data) {
                         if (params.data.online === "Oui") {
-                            // return "<a href='http://www.theses.fr/"+params.data.id+"/document' target='_blank' style='color: #0000EE;'>"+params.data.titre+"</a>";
                             return "<a href='http://www.theses.fr/"+params.data.id+"' target='_blank' style='color: #0000EE;'>"+params.data.titre+"</a>";
                         }
                         return params.data.titre;
@@ -171,6 +219,7 @@ https://opencagedata.com/tutorials/geocode-in-php
             {field: "Statut", flex: .8, cellClassRules: ragCellClassRules, cellRenderer: ragRenderer},
             {field: "Soutenance", flex: 1}
         ];
+
 
         //Calcul des résultats
         <?php
@@ -187,16 +236,17 @@ https://opencagedata.com/tutorials/geocode-in-php
         $recherche = $_POST['searchbar'];
         $recherche = "%".$recherche."%";
         $type = $_POST['choice-search'];
+        //Requête différente selon le type de recherche
         if ($type == 'all') {
-            $sth = $dbh->prepare('SELECT * FROM these WHERE 
+            $sth = $dbh->prepare('SELECT * FROM these2 WHERE 
                       auteur LIKE :recherche OR
                       titre LIKE :recherche OR
                       auteur LIKE :recherche OR
                       directeur_these_pn LIKE :recherche;');
-        } else $sth = $dbh->prepare('SELECT * FROM these WHERE '.$type.' LIKE :recherche;');
+        } else $sth = $dbh->prepare('SELECT * FROM these2 WHERE '.$type.' LIKE :recherche;');
         $sth->bindParam(':recherche', $recherche);
 
-        // Insertion de la requête SQL dans la BDD
+        //Initialisation des variables
         $rows = array();
         $rowHCtheses = array();
         $rowHConline = array(
@@ -207,11 +257,13 @@ https://opencagedata.com/tutorials/geocode-in-php
             array('En cours', 0),
             array('Soutenue', 0)
         );
+        $lstDiscipline = array();
+        $lstLangues = array();
         $lstCoords = array();
 
         //Résultats de la requête
         if ($sth->execute()) {
-            //Si ça se passe bien alors on crée la ligne du tableau
+            //Si ça se passe bien alors on va calculer différentes choses pour chaque ligne
             $result = $sth->fetchAll();
             foreach ($result as $row) {
                 //Formatage des lignes nécessaires
@@ -240,23 +292,28 @@ https://opencagedata.com/tutorials/geocode-in-php
                 //Construction des datas pour les graphiques HighCharts :
 
                 //Pour le graphe nb de thèses:
-                if ($row[11] == '01/01/1970') continue;    //On ignore la date 1970 par défaut
-                $annee = explode("/", $row[11])[2];
-                $exist = false;
-                for ($i=0; $i<count($rowHCtheses); $i++) {
-                    if ($rowHCtheses[$i][0] == $annee) {
-                        $rowHCtheses[$i][1] += 1;
-                        $exist = true;
+                if (mb_substr_count($row[11], "/") == 2) {  //Si la date est mal formatée next
+                    $annee = explode("/", $row[11])[2];
+                    $exist = false;
+                    //Si une array avec [annee, nb] est déjà présente pour cette année, alors on incrémente nb
+                    for ($i=0; $i<count($rowHCtheses); $i++) {
+                        if ($rowHCtheses[$i][0] == $annee) {
+                            $rowHCtheses[$i][1] += 1;
+                            $exist = true;
+                        }
+                    }
+                    //Sinon on crée l'array [annee, 1]
+                    if ($exist == false) {
+                        array_push($rowHCtheses, array($annee, 1));
                     }
                 }
-                if ($exist == false) {
-                    array_push($rowHCtheses, array($annee, 1));
-                }
+
 
                 //Pour le camambert thèses online
                 $indexOnline = $row[14] == 'Oui' ? 1 : 0;
                 //On incrémente le nombre de thèses correspondant à online Oui ou Non
                 $rowHConline[$indexOnline][1] += 1;
+
 
                 //Pour le camambert thèses soutenue
                 $indexSoutenue = $row[9] == 'Soutenue' ? 1 : 0;
@@ -264,21 +321,58 @@ https://opencagedata.com/tutorials/geocode-in-php
                 $rowHCsoutenue[$indexSoutenue][1] += 1;
 
 
-                // ----- PARTIE CARTOGRAPHIE -----
-                $test = '';
+                //Pour le top 10 des disciplines
+                $discipline = $row[8];
+                $found = false;
+                if ($discipline != "") {
+                    //Si discipline est présent, alors on incrémente nb
+                    for ($i=0; $i<count($lstDiscipline); $i++) {
+                        if ($lstDiscipline[$i]["name"] == $discipline) {
+                            $lstDiscipline[$i]["y"] += 1;
+                            $lstDiscipline[$i]["z"] += 1;
+                            $found = true;
+                        }
+                    }
+                    //On ajoute à l'array un array [discipline, nb] si discipline n'est pas encore présent
+                    if ($found == false) {
+                        array_push($lstDiscipline, array('name' => $discipline, 'y' => 1, 'z' => 1));
+                    }
+                }
 
-                $sql = "SELECT coord_x, coord_y FROM coordonnees WHERE id_etablissement='$row[7]';";
+
+                //Pour les langues les plus populaires
+                $langue = $row[12];
+                $added = false;
+                if ($langue != "") {
+                    //Si langue est présent, alors on incrémente nb
+                    for ($i=0; $i<count($lstLangues); $i++) {
+                        if ($lstLangues[$i]["name"] == strtoupper($langue)) {
+                            $lstLangues[$i]["y"] += 1;
+                            $added = true;
+                        }
+                    }
+                    //On ajoute à l'array un array [langue, nb] si langue n'est pas encore présent
+                    if ($added == false) {
+                        array_push($lstLangues, array('name' => strtoupper($langue), 'y' => 1, 'selected' => false, 'sliced' => false));
+                    }
+                }
+
+
+
+
+
+                // ----- PARTIE CARTOGRAPHIE -----
+                //Récupération des coordonnées + nom d'établissement pour ajouter des marqueurs sur la carte
+                $sql = "SELECT coord_x, coord_y, etablissement FROM coordonnees WHERE id_etablissement='$row[7]';";
                 $req = $dbh->prepare($sql);
 
                 if ($req->execute()) {
                     while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
-                        array_push($lstCoords, array($row['coord_x'], $row['coord_y']));
-                        $test = $row;
+                        //Pas de doublons dans la liste des coord pour ne pas surcharger la map
+                        if (!in_array(array($row['coord_x'], $row['coord_y'], $row['etablissement']), $lstCoords)) {
+                            array_push($lstCoords, array($row['coord_x'], $row['coord_y'], $row['etablissement']));
+                        }
                     }
-
-                } else {
-                    $test = 'non';
-                    echo $req->errorInfo().'<br>';
                 }
             }
 
@@ -286,6 +380,19 @@ https://opencagedata.com/tutorials/geocode-in-php
             usort($rowHCtheses, function ($item1, $item2) {
                 return $item1[0] <=> $item2[0];
             });
+
+            //Top 10 des disciplines
+            usort($lstDiscipline, function ($item1, $item2) {
+                return $item2["y"] <=> $item1["y"];
+            });
+            $topDiscipline = array_slice($lstDiscipline, 0, 10);
+
+            //Langues les plus populaires
+            usort($lstLangues, function ($item1, $item2) {
+                return $item2["y"] <=> $item1["y"];
+            });
+//            $lstLangues[0]['selected'] = true;
+//            $lstLangues[0]['sliced'] = true;
         }
 
         //On écrit dans un .json les lignes résultées
@@ -294,13 +401,14 @@ https://opencagedata.com/tutorials/geocode-in-php
         fwrite($fp, json_encode($rows));
         fclose($fp);
         ?>
-        console.log(<?php echo json_encode($test); ?>)
+        console.log(<?php echo json_encode($lstLangues); ?>)
 
 
         //Ajout des marker sur la map
         let lstCoords = <?php echo json_encode($lstCoords); ?>;
         lstCoords.forEach(elem => {
-            L.marker(elem).addTo(map);
+            let marker = L.marker([elem[0],elem[1]], {title: elem[2]}).addTo(map);
+            marker.bindPopup(elem[2]);
         })
 
 
@@ -339,7 +447,8 @@ https://opencagedata.com/tutorials/geocode-in-php
         let datasNbTheses = <?php echo json_encode($rowHCtheses); ?>;
         let datasNbOnline = <?php echo json_encode($rowHConline); ?>;
         let datasNbSoutenue = <?php echo json_encode($rowHCsoutenue); ?>;
-
+        let datasTopDisciplines = <?php echo json_encode($topDiscipline); ?>;
+        let datasLangues = <?php echo json_encode($lstLangues); ?>;
 
 
 
@@ -434,7 +543,7 @@ https://opencagedata.com/tutorials/geocode-in-php
                     plotShadow: false,
                 },
                 title: {
-                    text: 'Proportion de<br>thèses en ligne<br>sur theses.fr',
+                    text: 'Visible en ligne',
                     align: 'center',
                     verticalAlign: 'middle',
                     y: 60
@@ -480,7 +589,7 @@ https://opencagedata.com/tutorials/geocode-in-php
                     plotShadow: false,
                 },
                 title: {
-                    text: 'Proportion de<br>thèses soutenue',
+                    text: 'Statut des thèses',
                     align: 'center',
                     verticalAlign: 'middle',
                     y: 60
@@ -519,17 +628,83 @@ https://opencagedata.com/tutorials/geocode-in-php
                     data: datasNbSoutenue
                 }]
             });
+            Highcharts.chart('topDisciplines', {
+                chart: {
+                    type: 'variablepie'
+                },
+                title: {
+                    text: 'Disciplines les plus populaires'
+                },
+                tooltip: {
+                    headerFormat: '',
+                    pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b><br/>' +
+                            'Thèses ayant comme discipline {point.name}: <b>{point.y}</b><br/>'
+                },
+                series: [{
+                    minPointSize: 10,
+                    innerSize: '20%',
+                    zMin: 0,
+                    name: 'discipline',
+                    data: datasTopDisciplines
+                }]
+            });
+            Highcharts.chart('topLangues', {
+                chart: {
+                    styledMode: true
+                },
+                title: {
+                    text: 'Langues les plus populaires'
+                },
+                tooltip: {
+                    pointFormat: 'Langue {point.name}: <b>{point.percentage:.1f}%</b> ({point.y})'
+                },
+                series: [{
+                    type: 'pie',
+                    allowPointSelect: true,
+                    keys: ['name', 'y', 'selected', 'sliced'],
+                    data: datasLangues,
+                    showInLegend: true
+                }]
+            });
         });
 
-        //Affichage a utilisateur le nombre de résultats
-        document.getElementById("nbResultats").innerHTML = parseInt(<?php echo count($rows); ?>)+" résultats";
+
+
+        //Affichage a l'utilisateur du résumé de la recherche
+        <?php
+            // On formate bien le texte du type de recherche
+            switch($type) {
+                case "all":
+                    $type = "Tout";
+                    break;
+
+                case "auteur":
+                    $type = "Auteur";
+                    break;
+
+                case "titre":
+                    $type = "Titre";
+                    break;
+
+                case "discipline":
+                    $type = "Discipline";
+                    break;
+
+                case "directeur_these_pn":
+                    $type = "Directeur de la thèse";
+                    break;
+            }
+        ?>
+        document.getElementById("nbResultats").innerHTML = parseInt(<?php echo count($rows); ?>).toString()+
+            " thèses trouvées pour la recherche \"<?php echo $_POST['searchbar']; ?>\" dans {<?php echo $type; ?>}";
 
     </script>
 
 </main>
 
 <footer>
-    <p>Amalaric Le Forestier</p>
+    <p><a href="https://amalaric.dev" target="_blank"><i class="fas fa-user-graduate"></i> Amalaric Le Forestier</a></p>
+    <p><a href="../mentions.html" target="_blank">Mentions légales <i class="fas fa-file-contract"></i></a></p>
 </footer>
 
 </body>
